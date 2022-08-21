e172f3e392e4b091f8b352f93ee4dbc2b79fc794
jokerclay <2079209547@qq.com>
2022-08-21 08:09:55 -0400

    removed binary & updated readme

---
+++ 01_线性表/01_链表/main.c
@@ -0,0 +1,8 @@
+/*
+ * 关于链表的基本操作
+ *
+ */
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
---
+++ 01_线性表/01_链表/question/main.c
@@ -0,0 +1,61 @@
+/*
+ * 从顺序表中删除最小的元素（假设唯一）并由函数返回被删除元素的值，空出的位置由
+ * 最后一个元素填补，若顺序表为空，则显示出错误信息并退出运行
+ *
+ */
+
+#include <time.h>
+#include <stdbool.h>
+#include "./vector.h"
+
+int answer(Vector *vec, int value) {
+
+    if (vec ==NULL)  {
+        printf("Vector doesn't exist !");
+        exit(0);
+    }
+
+    if (vec->length == 0 ){
+        printf("Vector is empty!");
+        exit(0);
+    }
+
+    int index = 0;
+    value = vec->data[0];
+
+    for (int i = 1; i < vec->length; i++) {
+
+        if (vec->data[i] < value) {
+            value = vec->data[i];
+            index  =  i;
+        }
+    }
+
+    vec->data[index] = vec->data[vec->length - 1];
+    vec->length--;
+    return value;
+}
+
+
+int main(void) 
+{
+    
+    Vector *vec  = init_vector(10);
+
+    srand(time(0));
+
+    for (int i = 0; i < 10; ++i) {
+        insert(vec, i, rand() % 100);
+    }
+
+    get_vec_status(vec);
+
+    int value = 0;
+    int ans = answer(vec,value);
+
+
+    get_vec_status(vec);
+    printf("====> [MIN VALUE]: %d\n", ans);
+
+    return 0;
+}
